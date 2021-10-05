73d3395133a7c9d9b3b0b1d5eaf3fbe3a21224f0
debramacher <benedict.ramacher@brenntag.de>
2021-10-05 13:44:12 +0200

    Initial commit

---
+++ src/test/java/com/udemy_ramacher/simplewebapp/SimpleWebAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.udemy_ramacher.simplewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebAppApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
