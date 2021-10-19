c7785a474bba6cf0bdec3a4daa331b1df6583650
debramacher <benedict.ramacher@brenntag.de>
2021-10-19 10:09:20 +0200

    Lecture 23 done

--- src/main/java/com/udemy_ramacher/simplewebapp/controllers/AuthorController.java
+++ src/main/java/com/udemy_ramacher/simplewebapp/controllers/AuthorController.java
@@ -4,18 +4,14 @@ import com.udemy_ramacher.simplewebapp.repositories.AuthorRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AuthorController
 {
-
 	private final AuthorRepository authorRepository;
 
-
-	public AuthorController(AuthorRepository authorRepository)
-	{
-		this.authorRepository = authorRepository;
-	}
+	public AuthorController(AuthorRepository authorRepository) {this.authorRepository = authorRepository;}
 
 	@GetMapping("/authors")
 	public String getAuthors(Model model)
--- src/main/java/com/udemy_ramacher/simplewebapp/controllers/BookController.java
+++ src/main/java/com/udemy_ramacher/simplewebapp/controllers/BookController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BookController
 {
-
 	private final BookRepository bookRepository;
 
 	public BookController(BookRepository bookRepository)
